6e5c0a15ec206e2ec0fe54034297c89d5a89ee2f
a5wap123 <a5wap123@gmail.com>
2015-10-13 16:14:32 +0700

    add lotication

--- hpproj3/MainPage.xaml.cs
+++ hpproj3/MainPage.xaml.cs
@@ -8,20 +8,52 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using hpproj3.Resources;
+using Windows.Devices.Geolocation;
 
 namespace hpproj3
 {
     public partial class MainPage : PhoneApplicationPage
     {
         // Constructor
+        public System.Device.Location.GeoCoordinate MyAdrress = new System.Device.Location.GeoCoordinate();
         public MainPage()
         {
             InitializeComponent();
-
+            
+           
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await FillAdress();
+            this.gMap.SetView(MyAdrress, ZoomLevel);
+        }
+        private async System.Threading.Tasks.Task FillAdress()
+        {
+            Geolocator geolocator = new Geolocator();
+            geolocator.DesiredAccuracyInMeters = 50;
 
+            try
+            {
+                Geoposition geoposition = await geolocator.GetGeopositionAsync(
+                     maximumAge: TimeSpan.FromMinutes(5),
+                     timeout: TimeSpan.FromSeconds(10)
+                    );
+
+                //With this 2 lines of code, the app is able to write on a Text Label the Latitude and the Longitude, given by {{Icode|geoposition}}
+                MyAdrress.Latitude = geoposition.Coordinate.Latitude;
+                MyAdrress.Longitude = geoposition.Coordinate.Longitude;
+            }
+            //If an error is catch 2 are the main causes: the first is that you forgot to include ID_CAP_LOCATION in your app manifest. 
+            //The second is that the user doesn't turned on the Location Services
+            catch (Exception ex)
+            {
+                //exception
+            }
+
+        }
+        
         public double ZoomLevel
         {
             get { return (double)GetValue(ZoomLevelProperty); }
@@ -52,6 +84,11 @@ namespace hpproj3
         }
 
         private void Map_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+        }
+
+        private void gMap_Hold(object sender, System.Windows.Input.GestureEventArgs e)
         {
 
         }
